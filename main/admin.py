5cd73c3ca917eef6b713d5aae387dda9669f6b05
y-ooe <fko2447054@stu.o-hara.ac.jp>
2024-10-16 17:37:31 +0900

    [add]admin追加

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Currency, Wallet, CurrencyInventory
 
 # Register your models here.
+admin.site.register(Currency)
+admin.site.register(Wallet)
+admin.site.register(CurrencyInventory)
