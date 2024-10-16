08c9331902be84484c25ccc6a4e45bea9242331f
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-10-16 17:28:31 +0900

    [fix]models.pyを修正

--- accounts/models.py
+++ accounts/models.py
@@ -5,25 +5,3 @@ from django.contrib.auth.models import AbstractUser
 #ユーザーテーブル
 class User(AbstractUser):
     pass
-
-#通貨テーブル
-class Currency(models.Model):
-    denomination = models.IntegerField(max_length=10)
-    image_url = models.URLField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-#ユーザーの財布テーブル
-class Wallet(models.Model):
-    user_id = models.ForeignKey(Currency, on_delete=models.CASCADE)
-    total_amount = models.IntegerField(max_length=10)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-#財布内の通貨テーブル
-class CurrencyInventory(models.Model):
-    currency_id = models.ForeignKey(Currency, on_delete=models.CASCADE)
-    wallet_id = models.ForeignKey(Wallet, on_delete=models.CASCADE)
-    quantity = models.IntegerField(max_length=3)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+#通貨テーブル
+class Currency(models.Model):
+    denomination = models.IntegerField(max_length=10)
+    image_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+#ユーザーの財布テーブル
+class Wallet(models.Model):
+    user_id = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    total_amount = models.IntegerField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+#財布内の通貨テーブル
+class CurrencyInventory(models.Model):
+    currency_id = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    wallet_id = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    quantity = models.IntegerField(max_length=3)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
