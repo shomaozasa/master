b45e9c59e1530ee9c15cd083d3921f755d619ff0
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-10-16 17:48:19 +0900

    [fix]エラー修正

--- main/models.py
+++ main/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-
+from accounts.models import User
 # Create your models here.
 
 #通貨テーブル
 class Currency(models.Model):
     denomination = models.IntegerField()
-    image_url = models.URLField()
+    image_url = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 #ユーザーの財布テーブル
 class Wallet(models.Model):
-    user_id = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     total_amount = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
