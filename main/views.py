7e4730cffbce11e961d2cbb53555c7bcdb794887
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-10-24 18:23:06 +0900

    [update]index編集

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+class TemplateView(TemplateView):
+    template_name = 'main/index.html' # 画面を表示するテンプレート名
+    
