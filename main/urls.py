1c67fe22ea1d03c0b47e400d029b3c560c94c41d
y-ooe <fko2447054@stu.o-hara.ac.jp>
2024-10-31 18:06:58 +0900

    signup.html

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 from .views import *
+
+app_name = "main"
 urlpatterns = [
     path("", TemplateView.as_view(), name="template"),
+    path("/signup", SignupView.as_view(), name = "signup"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,4 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class TemplateView(TemplateView):
     template_name = 'main/index.html' # 画面を表示するテンプレート名
-    
+
+class SignupView(TemplateView):
+    template_name = 'main/signup.html'
